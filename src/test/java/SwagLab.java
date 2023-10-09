79db5f91875b9d38fa931064b446538ebe577981
D1GiAsto3link <Samad@digilink.africa>
2023-10-09 10:42:33 +0200

    Webdriver socket

--- src/test/java/SwagLab.java
+++ src/test/java/SwagLab.java
@@ -1,4 +1,7 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class SwagLab {
@@ -8,7 +11,28 @@ public class SwagLab {
 
 
     @Test(priority = 1)
+    //this will open the browser up (basically connecting to the web)
     public void SetUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @Test (priority = 2)
+    public void openbrowsersite() throws InterruptedException {
+        driver.get("https://www.saucedemo.com");
+        Thread.sleep(2000);
+
+    }
+
+    @Test (priority = 3)
+    public void Login(){
+        driver.findElement(By.xpath("//*[@id=\"user-name\"]"))
+                .sendKeys("standard_user");
+        driver.findElement(By.xpath("//*[@id=\"password\"]"))
+                .sendKeys("secret_sauce");
+
+        driver.findElement(By.id("login-button")).click();
+
 
     }
 
